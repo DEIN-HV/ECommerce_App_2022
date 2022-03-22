c272ee1730fafe1bc0b70346885dcc2065a2d3b5
DIEN HUYNH VINH <64977759+DEIN-HV@users.noreply.github.com>
2022-03-23 00:14:35 +0700

    ud

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -2,7 +2,7 @@ import Button from "../Form/Button";
 import "./styles.scss";
 import { signInWithGoogle, auth } from "../../firebase/utils";
 import { signInWithEmailAndPassword } from "firebase/auth"
-import { Component } from "react"
+import { Component, useState } from "react"
 import FormInput from "../Form/FormInput";
 import AuthWrapper from "../AuthWrapper";
 import { Link } from "react-router-dom";
@@ -13,99 +13,103 @@ const initialState = {
     errors: ""
 }
 
-class SignIn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...initialState
-        }
+const SignIn = props => {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         ...initialState
+    //     }
 
-        this.handleChange = this.handleChange.bind(this)
+    //     this.handleChange = this.handleChange.bind(this)
+    // }
+
+    // handleChange(e) {
+    //     const { name, value } = e.target;
+    //     this.setState({
+    //         [name]: value,
+    //     })
+    // }
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    const resetForm = () => {
+        setEmail("");
+        setPassword("");
+        setErrors([]);
     }
 
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value,
-        })
-    }
-
-    handleSubmit = async e => {
+    const handleSubmit = async e => {
         e.preventDefault();
+        console.log(email, password)
 
-        const { email, password } = this.state;
         try {
             await signInWithEmailAndPassword(auth, email, password)
                 .then(() => {
-                    this.setState = {
-                        ...initialState
-                    }
+                    resetForm();
                 })
                 .catch(() => {
                     const err = ["Email or Password is wrong"];
-                    this.setState({
-                        ...initialState,
-                        errors: err,
-                    })
+                    setErrors(err)
                 })
 
         } catch (error) {
-            console.log("error", error);
+            console.log(error);
 
         }
     }
-    render() {
-        const { email, password, errors } = this.state;
-        return (
-            <AuthWrapper headline="login page">
 
-                <form className="formWrap" onSubmit={this.handleSubmit}>
-                    <FormInput type="email"
-                        name="email"
-                        value={email}
-                        placeholder="Email"
-                        handleChange={this.handleChange}
+    return (
+        <AuthWrapper headline="login page">
 
-                    />
-                    <FormInput type="password"
-                        name="password"
-                        value={password}
-                        placeholder="Password"
-                        handleChange={this.handleChange}
-                    />
+            <form className="formWrap" onSubmit={handleSubmit}>
+                <FormInput type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    handleChange={(e) => setEmail(e.target.value)}
 
-                    {/* error message */}
-                    {errors &&
-                        <ul className="errorMess">
-                            {errors.map((err) => (
-                                <li>{err}</li>
-                            ))}
-                        </ul>
-                    }
+                />
+                <FormInput type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    handleChange={(e) => setPassword(e.target.value)}
+                />
 
-                    <Button type="submit">
-                        Login
-                    </Button>
+                {/* error message */}
+                {errors &&
+                    <ul className="errorMess">
+                        {errors.map((err) => (
+                            <li>{err}</li>
+                        ))}
+                    </ul>
+                }
 
-                    {/* SignIn with google */}
-                    <div className="socialSignIn">
-                        <div className="row">
-                            <Button onClick={signInWithGoogle}>
-                                Sign in with Google
-                            </Button>
-                        </div>
+                <Button type="submit">
+                    Login
+                </Button>
+
+                {/* SignIn with google */}
+                <div className="socialSignIn">
+                    <div className="row">
+                        <Button onClick={signInWithGoogle}>
+                            Sign in with Google
+                        </Button>
                     </div>
+                </div>
 
-                    <div className="forgotPassword">
-                        <Link to="/recovery">
-                            <span className="forgotPasswordText">Forgot Password</span>
-                        </Link>
-                    </div>
-                </form>
-            </AuthWrapper>
+                <div className="forgotPassword">
+                    <Link to="/recovery">
+                        <span className="forgotPasswordText">Forgot Password</span>
+                    </Link>
+                </div>
+            </form>
+        </AuthWrapper>
 
-        )
-    }
+    )
 }
+
 
 export default SignIn
